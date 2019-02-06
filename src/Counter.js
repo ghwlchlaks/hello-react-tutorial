418af418765370b7cab9762e5a9da45bd72041f8
ghwlchlaks <ghwlchlaks@naver.com>
2019-02-06 20:04:54 +0900

    props state 개념

---
+++ src/Counter.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+export default class Counter extends Component {
+  state = {
+    number: 0
+  }
+  handleIncrease = () => {
+    this.setState(
+      (state) => ({
+        number: state.number + 1
+      })
+    );
+  }
+  handleDecrease = () => {
+    this.setState(
+      ({number}) => ({
+        number: number - 1
+      })
+    );
+  }
+  render() {
+    return (
+      <div>
+        <h1>카운터</h1>
+        <div>값 : {this.state.number}</div>
+        <button onClick={this.handleIncrease}>+</button>
+        <button onClick={this.handleDecrease}>-</button>
+      </div>
+    )
+  }
+}
