ba3b87dc2ca22ca8fabf474119f8c88364f43a9b
ghwlchlaks <ghwlchlaks@naver.com>
2019-02-06 20:08:51 +0900

    자식 컴포넌트 오류 발생시 처리
    componentDidCatch(error, info)

--- src/App.js
+++ src/App.js
@@ -4,14 +4,33 @@ import './App.css';
 import Name from './Name';
 import Counter from './Counter';
 
+const Problem = () => {
+  throw (new Error('bug'));
+  return (
+    <div>
+
+    </div>
+  )
+}
+
 class App extends Component {
+  state = {
+    error: false
+  }
   render() {
+    if (this.state.error) return (<h1>자식 컴포넌트에서 에러 발생!</h1>)
     return (
       <Fragment>
+        <Problem></Problem>
         <Name name="test"></Name>
         <Counter></Counter>
       </Fragment>
     );
+  }
+  componentDidCatch(error, info) {
+    this.setState({
+      error: true
+    })
   }
 }
 
